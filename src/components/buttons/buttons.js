b5b630b0868dd1a1d81043312737f0d20d2f9cfe
viljar <viljarsalu@gmail.com>
2015-10-12 16:36:14 +0300

    javascript production code test

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -3,16 +3,19 @@
     console.log('hallo from buttons.js');
     // useage https://github.com/SC5/sc5-styleguide#providing-additional-javascript
     // target scope e.originalEvent.detail.elements
-    var p = 0;
     $(window).bind("styleguide:onRendered", function(e) {
-        console.log('onrendered:',p,e.originalEvent.detail.elements.find('.button'));
-        p++;
-        //var btn = document.querySelectorAll('.button');
-        var btn = $('.button');
-        e.originalEvent.detail.elements.find('.button').click(function(e){
+        var el = e.originalEvent.detail.elements.find('.button');
+       init(el);
+    });
+
+    // init funktsioonis saan kasutada production lahenemist koodis
+    function init(el){
+        var btn = el;
+        console.log(btn,btn.length, 'test');
+        btn.click(function(e){
             console.log('hallo button from buttons.js file');
             e.preventDefault();
         });
-    });
+    }
 
 })();
